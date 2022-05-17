82c22298b4f318ba62bda2ff6b1e12e8751b92df
Schwelle76 <mattes.zens@web.de>
2022-05-17 13:24:09 +0200

    Positioned elements roughly. Ref: #15

--- screens/ImportCalendarScreen.tsx
+++ screens/ImportCalendarScreen.tsx
@@ -1,26 +1,35 @@
 import React from 'react'
-import { Text, Button, Image} from "react-native"
+import { StyleSheet, Text, Button, Image, View } from "react-native"
+import { Dimensions } from 'react-native'
+
+const screenSize = Dimensions.get('screen');
 
 const ImportCalendarScreen = () => {
     return (
-        <Text>
-            <Image source={require('../assets/images/calendar-icon.png')} />
-            Importiere deinen Kalender, damit wir deine freien Zeiten eintragen können.
-            <Button
-                onPress={importCalendar}
-                title="Kalender importieren"
-                color="#841584"
-                accessibilityLabel="Learn more about this purple button"
-            />
+        <View style={styles.container}>
+            <View style={styles.upperContainer}>
 
-            <Button
-                onPress={enterFreeTimeManually}
-                title="oder manuell eintragen"
-                color="#841584"
-                accessibilityLabel="Learn more about this purple button"
-            />
-        </Text>
-        
+                <Image source={require('../assets/images/calendar-icon.png')} />
+                <Text>
+                    Importiere deinen Kalender, damit wir deine freien Zeiten eintragen können.
+                </Text>
+            </View>
+            <View style={styles.lowerContainer}>
+                <Button 
+                    onPress={importCalendar}
+                    title="Kalender importieren"
+                    color="#841584"
+                    accessibilityLabel="Learn more about this purple button"
+                />
+                <Button
+                    onPress={enterFreeTimeManually}
+                    title="oder manuell eintragen"
+                    color="#841584"
+                    accessibilityLabel="Learn more about this purple button"
+                />
+            </View>
+
+        </View>
     )
 }
 
@@ -31,5 +40,44 @@ const importCalendar = () => {
 const enterFreeTimeManually = () => {
     console.log("enterFreeTimeManually");
 }
+
+const styles = StyleSheet.create({
+    container: {
+        height: "100%",
+        width: "100%",
+        flexDirection: "column",
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        alignContent: 'center',
+        backgroundColor: 'blue'
+    },
+
+    upperContainer: {
+        marginVertical: "15%",
+        height: "50%",
+        width: "100%",
+        justifySelf: 'center',
+        alignItems: 'center',
+        backgroundColor: 'green'
+    },
+
+    lowerContainer: {
+        marginBottom: "15%",
+        height: "20%",
+        width: "100%",
+        position: 'relative',
+        alignself: 'end',
+        justifyContent: 'space-around',
+        backgroundColor: 'yellow'
+    },
+
+    text: {
+        color: 'green'
+    },
+
+    button: {
+        margin: '3%'
+    }
+})
 
 export default ImportCalendarScreen
