4a9bfa511e6ce5fcad2fffb4b79bacf605ed83ae
Eric Goerens <goerens.eric@gmail.com>
2022-05-19 12:17:09 +0200

    Feature/text component (#32)
    * feat: creates text component and implements it to the other screens
    
    * fix: fixes issues with css in Text and Header component
    
    * feat: implements LoadingOverlay

--- screens/ImportCalendarScreen.tsx
+++ screens/ImportCalendarScreen.tsx
@@ -1,74 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Image, View, TouchableOpacity } from 'react-native'
 import Button from '../components/Button'
 import Text from '../components/Text'
 import Colors from '../constants/Colors'
 import { Dimensions } from 'react-native'
+import LoadingOverlay from '../components/LoadingOverlay'
 
 const screenDimensions = Dimensions.get('screen')
 
-
 const ImportCalendarScreen = () => {
-	return (
-		<View style={styles.container}>
-			<View style={styles.upperContainer}>
-				<Image source={require('../assets/images/calendar-icon.png')} />
-				<Text style={styles.text}>
-					Importiere deinen Kalender, damit wir deine freien Zeiten eintragen können.
-				</Text>
-			</View>
-			<View style={styles.lowerContainer}>
-				<Button onPress={importCalendar}>Kalender importieren</Button>
-				<TouchableOpacity onPress={enterFreeTimeManually}>
-					<Text style={styles.clickableText}>oder manuell eintragen</Text>
-				</TouchableOpacity>
-			</View>
-		</View>
-	)
-}
+    const [isLoading, setLoading] = useState(false)
 
-const importCalendar = () => {
-	console.log('import Calendar')
-}
+    const importCalendar = () => {
+        console.log("import Calendar");
+        setLoading(true)
+        setTimeout(() => { setLoading(false) }, 1500)
+    }
 
-const enterFreeTimeManually = () => {
-	console.log('enterFreeTimeManually')
+    const enterFreeTimeManually = () => {
+        console.log("enterFreeTimeManually");
+    }
+
+    return (
+        <View style={styles.container}>
+            <LoadingOverlay show={isLoading} />
+            <View style={styles.upperContainer}>
+                <Image source={require('../assets/images/calendar-icon.png')} />
+                <Text style={styles.text}>
+                    Importiere deinen Kalender, damit wir deine freien Zeiten eintragen können.
+                </Text>
+            </View>
+            <View style={styles.lowerContainer}>
+                <Button onPress={importCalendar}>Kalender importieren</Button>
+                <TouchableOpacity onPress={enterFreeTimeManually}>
+                    <Text style={styles.clickableText}>oder manuell eintragen</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-	container: {
-		width: screenDimensions.width,
-		height: screenDimensions.height,
-		flexDirection: 'column',
-		alignItems: 'center',
-		alignContent: 'center',
-	},
+    container: {
+        width: screenDimensions.width,
+        height: screenDimensions.height,
+        flexDirection: 'column',
+        alignItems: 'center',
+        alignContent: 'center',
+    },
 
-	upperContainer: {
-		marginTop: '20%',
-		justifyContent: 'space-around',
-		justifySelf: 'center',
-		alignItems: 'center',
-	},
+    upperContainer: {
+        marginTop: '20%',
+        justifyContent: 'space-around',
+        justifySelf: 'center',
+        alignItems: 'center',
+    },
 
-	lowerContainer: {
-		marginTop: '20%',
-		position: 'relative',
-		justifyContent: 'space-between',
-	},
+    lowerContainer: {
+        marginTop: '20%',
+        position: 'relative',
+        justifyContent: 'space-between',
+    },
 
-	text: {
-		textAlign: 'center',
-		marginTop: '10%',
-	},
+    text: {
+        textAlign: 'center',
+        marginTop: '10%',
+    },
 
-	clickableText: {
-		fontSize: 20,
-		fontFamily: 'cantarell-regular',
-		color: Colors.primary,
-		textAlign: 'center',
-		textDecorationLine: 'underline',
-	},
+    clickableText: {
+        fontSize: 20,
+        fontFamily: 'cantarell-regular',
+        color: Colors.primary,
+        textAlign: 'center',
+        textDecorationLine: 'underline',
+    },
 
 })
 
